1438d8e2f46882cb14ee393746b19c5ac0970b5d
Andres Navarro <contact@andresnav.com>
2023-02-22 15:41:26 +0100

    Initial code upload

---
+++ utils.h
@@ -0,0 +1,24 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <stdint.h>
+
+/********************************************************************
+ * Constant definitions
+ ********************************************************************/
+#define IP_ADDRESS_LENGTH 32
+
+/********************************************************************
+ * Generate a netmask of length prefixLength
+ ********************************************************************/
+void getNetmask(int prefixLength, int *netmask);
+
+/********************************************************************
+ * Example of a very simple hash function using the modulus operator
+ * For more info: https://gist.github.com/cpq/8598442
+ ********************************************************************/
+int hash(uint32_t IPAddress, int sizeHashTable);
+
+// RL Lab 2020 Switching UC3M
+
+#endif // !UTILS_H
